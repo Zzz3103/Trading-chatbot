d1efaf4bf400eaed978a6695536b2f783401fcea
zz4353 <vmkqa2@gmail.com>
2025-07-23 10:12:18 +0700

    fix bug index_data()

--- src/index_data.py
+++ src/index_data.py
@@ -3,7 +3,9 @@ import json
 from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance, PointStruct
-from utils import hash_directory
+from utils import hash_directory, convert_to_markdown, get_files_in_directory
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 class Vector_Store:
@@ -26,22 +28,22 @@ class Vector_Store:
 
         # Hash toàn bộ thư mục data
         old_hash = ""
-        old_hash_file = os.path.join(path, "hash.txt")
+        old_hash_file = os.path.join(path, "hash.hsh")
         if os.path.exists(old_hash_file):
             with open(old_hash_file, "r") as f:
                 old_hash = f.read().strip()
 
-        hash = hash_directory(path, "hash.txt")
-
-        with open(old_hash_file, "w") as f:
-            f.write(hash)
+        hash = hash_directory(path, "hash.hsh")
 
         # Nếu hash không thay đổi thì không cần cập nhật
         if old_hash == hash:
             print("Data has not changed, skipping indexing.")
             return
 
-        # Nhập lại dữ liệu.
+        with open(old_hash_file, "w") as f:
+            f.write(hash)
+
+        # Lấy số chiều của vector từ mô hình embedding
         sample_vector = self.embedding_model.encode("test") 
         dim = sample_vector.shape[0]
 
@@ -50,41 +52,43 @@ class Vector_Store:
                     collection_name=self.collection_name,
                     vectors_config=VectorParams(size=dim, distance=Distance.EUCLID),
                 )
-        id = 0
 
-        files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+        idx = 0 
+        files = get_files_in_directory(path)
         for f in files:
-            if f.endswith(".json"):
+            if f.endswith(".docx") or f.endswith(".txt"):
                 print(f"Processing file: {f}")
-                data = {}
-                with open(os.path.join(path, f), "r", encoding="utf-8") as f1:
-                    data = json.load(f1)
+                markdown_text = convert_to_markdown(f)
+                docs = [Document(page_content=markdown_text)]
+                text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                    chunk_size=512, chunk_overlap=126
+                )
+                text = text_splitter.transform_documents(docs)
+            else:
+                # Bỏ qua các file khác
+                continue
+        
+            embeddings = self.embedding_model.encode([doc.page_content for doc in text])
 
-                labels = list(data.keys())
+            points = [
+                PointStruct(
+                    id=i + idx,
+                    vector=embeddings[i].tolist(),
+                    payload={"text": text[i].page_content},
+                )
+                for i in range(len(text))
+            ]
+            idx += len(points)
 
-
-
-                label_embeddings = self.embedding_model.encode(labels)
-
-                points = [
-                    PointStruct(
-                        id=i,
-                        vector=label_embeddings[i].tolist(),
-                        payload={"label": labels[i], "text": data[labels[i]]}
-                    )
-                    for i in range(id, id + len(labels))
-                ]
-                id += len(points)
-
-                # Chia nhỏ thành các batch để tránh lỗi quá tải
-                BATCH_SIZE = 100
-                for i in range(0, len(points), BATCH_SIZE):
-                    batch = points[i:i + BATCH_SIZE]
-                    self.qdrant_client.upsert(
-                        collection_name=self.collection_name,
-                        points=batch
-                    )
-                    print(f"Upserted batch {i // BATCH_SIZE + 1} of {len(points) // BATCH_SIZE + 1}")
+            # Chia nhỏ thành các batch để tránh lỗi quá tải
+            BATCH_SIZE = 100
+            for i in range(0, len(points), BATCH_SIZE):
+                batch = points[i:i + BATCH_SIZE]
+                self.qdrant_client.upsert(
+                    collection_name=self.collection_name,
+                    points=batch
+                )
+                print(f"Upserted batch {i // BATCH_SIZE + 1} of {len(points) // BATCH_SIZE + 1}")
 
     def search(self, query, top_k=5):
         query_vector = self.embedding_model.encode([query])[0]
@@ -95,14 +99,14 @@ class Vector_Store:
             limit=top_k
         )
 
-        return [(hit.payload["label"], hit.payload["text"], hit.score) for hit in hits]
+        return [(hit.payload["text"], hit.score) for hit in hits]
 
 
 if __name__ == "__main__":
     vector_store = Vector_Store()
     index_data = vector_store.index_data(path="data")
-    context = vector_store.search(query="SHF", top_k = 3)
-    for label, text, score in context:
-        print(f"Label: {label}, Text: {text}, Score: {score}")
+    context = vector_store.search(query="chứng khoán là gì", top_k = 3)
+    for text, score in context:
+        print(f"Text: {text}, Score: {score}")
         print("\n")
 
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
-import hashlib
 import os
+import hashlib
+from markitdown import MarkItDown
 
 def hash_directory(path, exclude_file=None):
     sha256 = hashlib.sha256()
@@ -18,3 +19,30 @@ def hash_directory(path, exclude_file=None):
                     sha256.update(chunk)  # Thêm nội dung
 
     return sha256.hexdigest()
+
+def get_files_in_directory(path):
+    files = []
+    for f in os.listdir(path):
+        path_f = os.path.join(path, f)
+        if os.path.isfile(path_f):
+            files.append(path_f)
+        elif os.path.isdir(path_f):
+            files.extend(get_files_in_directory(path_f))
+
+    return files
+
+def convert_to_markdown(path: str) -> str:
+    # Khởi tạo converter
+    converter = MarkItDown()
+
+    # Chuyển file .docx sang markdown
+    result = converter.convert(path)
+
+    return result.markdown
+
+
+
+if __name__ == "__main__":
+    input_file = "data/QA_STATIC.txt"
+    result = convert_to_markdown(input_file)
+    print(result)
